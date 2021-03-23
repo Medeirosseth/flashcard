ec3dedceac8ab812897c1609b28f6291c47aa741
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-03-23 12:55:10 -0700

    Add toggle clicking function
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>
    Co-authored-by: Tori West <victoria_west@ymail.com>

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function () {
+  $("#java").click(function () {
+    $("#javascript-definition").fadeToggle();
+    $("#java").fadeToggle();
+
+
+  });
+
+  $("#javascript-definition").click(function () {
+    $("#javascript-definition").fadeToggle();
+    $("#java").fadeToggle();
+
+
+  });
+});
